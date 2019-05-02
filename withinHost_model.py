55ac75e13bd9081418f82f79b0be50d39a16f06b
Suze Roostee <su2781ro-s@student.lu.se>
2019-05-02 11:31:29 -0400

    further details to within-host model added

--- withinHost_model.py
+++ withinHost_model.py
@@ -23,9 +23,14 @@ import matplotlib.pyplot as plt
 
 ###########################		start parameters	###############################
 
-n_0 = 1
-r0 = 0.1
-K = 3
+n1_0 = 1
+n2_0 = 1
+r1 = 0.5
+r2 = 0.8
+K1= 10
+K2 = 10
+alpha12 = 0.5
+alpha21 = 0.5
 
 #time
 ntimepoints = 1000
@@ -34,11 +39,73 @@ t = np.linspace(0,20, ntimepoints)
 
 ###########################		growth functions	###############################
 
-def malariaGrowth(n, t, r, K):
+def LotkaVolterraCompetition(n, t, r1, r2, K1, K2, alpha12, alpha21):
 	#calculate malaria growth in host
-	dndt = r*n*(1-(n/K))
-	return dndt
+	n1 = n0[0]
+	n2 = n0[1]
+	dn1dt = r1*n1*(1-((n1+alpha12*n2)/K1))
+	dn2dt = r2*n2*(1-((n2+alpha21*n1)/K2))
+	return dn1dt, dn2dt
 
-n0 = n_0
+n0 = (n1_0, n2_0)
 
-out = odeint(malariaGrowth, n0, t, args =(r, K))
+out = odeint(LotkaVolterraCompetition, n0, t, args =(r1, r2, K1, K2, alpha12, alpha21))
+
+n1, n2 = out.T
+
+###########################		Plot system	over time	################################ 
+
+plt.plot(t, n1, label="Strain 1")
+plt.plot(t, n2, label="Strain 2")
+plt.legend(loc="best")
+plt.xlabel("t")
+plt.grid()
+plt.show()
+
+###########################		Plot system	isoclines	################################ 
+plt.plot([0,K2/alpha21],[K2,0], color = "blue", label = "isocline for N1")
+plt.plot([0,max(K1, K2/alpha21)+2], [0,0], color = "blue")
+plt.plot([0,K1],[K1/alpha12,0], color = "orange", label = "isocline for N2")
+plt.plot([0,0], [0,max(K2, K1/alpha12)+2], color = "orange")
+plt.axis([-1, max(K1, K2/alpha21)+1, -1, max(K2, K1/alpha12)+1])
+plt.legend(loc="best")
+plt.grid()
+plt.show()
+
+############################  some playing around with direction fields		############
+
+
+# X = np.arange(-10, 10, 1)
+# Y = np.arange(-10, 10, 1)
+# U, V = np.meshgrid(X**2, Y**2)
+
+# fig, ax = plt.subplots()
+# q = ax.quiver(X, Y, U, V)
+# ax.quiverkey(q, X=0.3, Y=1.1, U=10,
+#              label='Quiver key, length = 10', labelpos='E')
+
+# plt.show()
+
+# xmax = 4.0
+# xmin = -xmax
+# D = 20
+# ymax = 4.0
+# ymin = -ymax
+# x = np.linspace(xmin, xmax, D)
+# y = np.linspace(ymin, ymax, D)
+# X, Y = np.meshgrid(x, y)
+# deg = np.arctan(Y**2 - X)
+# QP = plt.quiver(X,Y,np.cos(deg),np.sin(deg))
+# plt.show()
+
+
+###########################		Population model 	####################################
+
+def a_pop(c_a, n1, n2):
+	a = c_a*(n1+n2)
+	return a 
+
+def beta_pop(c_b, n1, n2, h):
+	beta = (c_b * (n1+n2))/(1+h*(n1+n2))
+	return beta
+
